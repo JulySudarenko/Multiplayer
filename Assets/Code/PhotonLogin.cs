cb120022f2ead6f5082d5b05ad4e89d933510b18
July Sudarenko <july@sudarenko.ru>
2022-04-25 10:14:58 +0300

    on lesson

---
+++ Assets/Code/PhotonLogin.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+
+namespace Code
+{
+    public class PhotonLogin : MonoBehaviourPunCallbacks
+    {
+        string gameVersion = "1";
+
+        private void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        void Start()
+        {
+            Connect();
+        }
+
+        public void Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+        
+        public override void OnConnectedToMaster()
+        {
+           base.OnConnectedToMaster(); 
+            Debug.Log($"Successeful connected to master! Region: {PhotonNetwork.CloudRegion}, ping: {PhotonNetwork.GetPing()}");
+            //Debug.Log("OnConnectedToMaster() was called by PUN");
+        }
+
+    }
+}
---
+++ Assets/Code/PlayfabLogin.cs
@@ -0,0 +1,31 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+namespace Code
+{
+    public class PlayfabLogin : MonoBehaviour
+    {
+        public void Start()
+        {
+            if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+            {
+                PlayFabSettings.staticSettings.TitleId = "2402C";
+            }
+
+            var request = new LoginWithCustomIDRequest {CustomId = "GeekBrainsLesson3", CreateAccount = true};
+            PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+        }
+
+        private void OnLoginSuccess(LoginResult result)
+        {
+            Debug.Log("Congratulations, you made successful API call!");
+        }
+
+        private void OnLoginFailure(PlayFabError error)
+        {
+            var errorMessage = error.GenerateErrorReport();
+            Debug.LogError($"Something went wrong: {errorMessage}");
+        }
+    }
+}
