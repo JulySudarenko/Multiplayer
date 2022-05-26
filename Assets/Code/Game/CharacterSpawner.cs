8e24b64303611241fec053fa73dcee49d3f97dbc
July Sudarenko <july@sudarenko.ru>
2022-05-26 11:26:18 +0300

    Android control

--- Assets/Code/Game/CharacterSpawner.cs
+++ Assets/Code/Game/CharacterSpawner.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 namespace Code.Game
 {
-    // internal class CharacterSpawner : MonoBehaviour
-    // {
-    //     [SerializeField] private GameObject _playerPrefab;
-    //
-    //     private void Start()
-    //     {
-    //         var number = PhotonNetwork.LocalPlayer.ActorNumber;
-    //         PhotonNetwork.Instantiate(_playerPrefab.name, transform.GetChild(Mathf.Abs(number)).position, Quaternion.identity);
-    //     }
-    // }
      internal class CharacterSpawner
      {
          private readonly Transform _spawnPlaces;
